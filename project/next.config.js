8c55ce6ff4cdbcfb0a32cfc5b0ce09eecde2af6a
Sangeet-web <tanmansangeet@gmail.com>
2026-06-01 17:39:59 +0100

    Update next.config.js

--- project/next.config.js
+++ project/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+module.exports = module.exports = nextConfig;
